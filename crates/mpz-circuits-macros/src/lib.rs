e5748eef340291aeed370608fc37298c7f59fb34
sinu <65924192+sinui0@users.noreply.github.com>
2024-05-07 18:17:30 -0800

    refactor(mpz-circuits): deprecate trace macro

--- crates/mpz-circuits-macros/src/lib.rs
+++ crates/mpz-circuits-macros/src/lib.rs
@@ -1,18 +1,7 @@
 mod evaluate;
-pub(crate) mod map;
 mod test;
-mod trace;
-pub(crate) mod traits;
-pub(crate) mod visitors;
 
 use proc_macro::TokenStream;
-
-const DEFAULT_SUFFIX: &str = "trace";
-
-#[proc_macro_attribute]
-pub fn trace(args: TokenStream, item: TokenStream) -> TokenStream {
-    trace::trace_impl(args, item)
-}
 
 #[proc_macro]
 pub fn evaluate(item: TokenStream) -> TokenStream {
