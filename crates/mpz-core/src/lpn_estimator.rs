42191090bd3426a21ab82c43a9515a5054c2501c
Xiang Xie <xiexiangiscas@gmail.com>
2024-07-18 16:46:48 +0800

    add bjmm_isd_binary

--- crates/mpz-core/src/lpn_estimator.rs
+++ crates/mpz-core/src/lpn_estimator.rs
@@ -6,7 +6,7 @@ use rug::{ops::Pow, Float};
 // The precision for security analysis.
 const PRECISION: u32 = 200;
 
-// The highest security level in our analysis.
+// The highest security level.
 const HIGHEST_SECURITY: usize = 256;
 
 /// Define the struct of LPN estimator.
@@ -28,8 +28,14 @@ impl LpnEstimator {
         res
     }
 
-    /// Compute the bit security under the Pooled Gauss attack, see page 37 in this [paper](https://eprint.iacr.org/2022/712.pdf).
-    pub fn security_under_pooled_gauss_binary(n: usize, k: usize, t: usize) -> f64 {
+    /// Compute the bit security under the Pooled Gauss attack, see page 37 in this [paper](https://eprint.iacr.org/2022/712.pdf). Note that it is the same for binary and larger fields.
+    ///
+    /// # Arguments.
+    ///
+    /// * `n` - The number of samples.
+    /// * `k` - The length of the secret.
+    /// * `t` - The Hammin weight of the error.
+    pub fn security_under_pooled_gauss(n: usize, k: usize, t: usize) -> f64 {
         let log_guess_prob = Self::cal_comb(n - k, t).log2() - Self::cal_comb(n, t).log2();
 
         let matrix_inversion_cost = (std::cmp::min(n - k, k) as f64).powf(2.8);
@@ -37,17 +43,12 @@ impl LpnEstimator {
         matrix_inversion_cost.log2() - log_guess_prob.to_f64()
     }
 
-    // Compute the fomulas inside the min function of Therem 14 in this [paper](https://eprint.iacr.org/2022/712.pdf).
+    // Compute the fomulas inside the min function of Theorem 14 in this [paper](https://eprint.iacr.org/2022/712.pdf).
     fn sub_sd_isd_binary(n: usize, k: usize, t: usize, l: usize, p: usize) -> f64 {
         let l_zero = Self::cal_comb((k + l) / 2 + 1, p / 2);
 
         let log_l_zero = l_zero.clone().log2();
         let log_s: Float = 2 * log_l_zero.clone() - l;
-
-        // If it has enough security, just break.
-        if log_l_zero > HIGHEST_SECURITY || log_s > HIGHEST_SECURITY {
-            return HIGHEST_SECURITY as f64;
-        }
 
         // Computes s = 1 << log_s.
         let s = Float::with_val(PRECISION, 2).pow(log_s.clone());
@@ -67,7 +68,7 @@ impl LpnEstimator {
         (cost.log2() - log_p).to_f64()
     }
 
-    // Minimize sub_SD_ISD_binary when fix p.
+    // Minimize sub_sd_isd_binary when fix p.
     fn min_sub_sd_isd_binary_with_fixed_p(n: usize, k: usize, t: usize, p: usize) -> f64 {
         let mut start = 1;
         let mut end = n - k;
@@ -102,6 +103,12 @@ impl LpnEstimator {
     }
 
     /// The security of the lpn parameters under SD_ISD attack for binary field. See Therem 14 in this [paper](https://eprint.iacr.org/2022/712.pdf).
+    ///
+    /// # Arguments.
+    ///
+    /// * `n` - The number of samples.
+    /// * `k` - The length of the secret.
+    /// * `t` - The Hammin weight of the error.
     pub fn security_under_sd_isd_binary(n: usize, k: usize, t: usize) -> f64 {
         let mut res = Self::sub_sd_isd_binary(n, k, t, 0, 0);
 
@@ -110,11 +117,199 @@ impl LpnEstimator {
             if min <= res {
                 res = min;
             }
-            if min > res + 30.0 {
-                break;
+        }
+        res
+    }
+
+    // Compute the fomulas inside the min function of Theorem 16 in this [paper](https://eprint.iacr.org/2022/712.pdf).
+    fn sub_bjmm_isd_binary(
+        n: usize,
+        k: usize,
+        t: usize,
+        p2: usize,
+        l: usize,
+        e1: usize,
+        e2: usize,
+        r1: usize,
+        r2: usize,
+        local_min: f64,
+    ) -> f64 {
+        assert!(p2 >= e2);
+        let p1 = 2 * (p2 - e2);
+        assert!(p1 >= e1);
+        let p = 2 * (p1 - e1);
+
+        let s3 = Self::cal_comb((k + l) / 2 + 1, p2 / 2);
+
+        let log_s3 = s3.clone().log2();
+        let log_c3: Float = s3.clone().log2() * 2 - r2;
+        let c3 = Float::with_val(PRECISION, 2).pow(log_c3.clone());
+
+        let log_c2: Float = log_c3.clone() * 2 - r1;
+        let c2 = Float::with_val(PRECISION, 2).pow(log_c2.clone());
+
+        assert!(k + l >= p2);
+        assert!(p2 >= e2);
+        let log_mu2 = Self::cal_comb(p2, e2).log2() + Self::cal_comb(k + l - p2, p2 - e2).log2()
+            - Self::cal_comb(k + l, p2).log2();
+
+        let log_s1_left = log_mu2 + log_c2.clone();
+        let log_s1_right = Self::cal_comb(k + l, p1).log2() - (r1 + r2);
+        let log_s1 = log_s1_left.min(&log_s1_right);
+
+        let log_c1: Float = log_s1 * 2 - l + r1 + r2;
+        let c1 = Float::with_val(PRECISION, 2).pow(log_c1.clone());
+
+        assert!(k + l >= p1);
+        assert!(p1 >= e1);
+        let log_mu1 = Self::cal_comb(p1, e1).log2() + Self::cal_comb(k + l - p1, p1 - e1).log2()
+            - Self::cal_comb(k + l, p1).log2();
+
+        let log_s_left = log_mu1 + log_c1;
+        let log_s_right = Self::cal_comb(k + l, p).log2() - l;
+
+        let log_s = log_s_left.clone().min(&log_s_right);
+
+        // Quick break
+        if (log_s3.to_f64() > local_min) || (log_c3.to_f64() > local_min) || (log_c2 > local_min) {
+            return HIGHEST_SECURITY as f64;
+        }
+
+        // Compute T_Gauss + (8*S3 + 4*C3 + 2*C2 + 2*C1) * N
+        let mut cost = Float::with_val(PRECISION, (n - k - l) * (n - k))
+            / Float::with_val(PRECISION, f64::log2((n - k - l) as f64));
+
+        cost += 8 * s3 + 4 * c3 + 2 * c2 + 2 * c1;
+        cost *= n;
+
+        // Compute log P(p,l).
+        assert!(n >= k + l);
+        assert!(t >= p);
+        let mut log_p = Self::cal_comb(n - k - l, t - p).log2() - Self::cal_comb(n, t).log2();
+
+        log_p += l + log_s;
+
+        // Quick break, the probability should be smaller than 1.
+        if log_p >= 0 {
+            return HIGHEST_SECURITY as f64;
+        }
+
+        (cost.log2() - log_p).to_f64()
+    }
+
+    // Minimize sub_bjmm_isd_binary with fixed p2 and l.
+    fn min_sub_bjmm_isd_binary_with_fixed_p2_and_l(
+        n: usize,
+        k: usize,
+        t: usize,
+        p2: usize,
+        l: usize,
+    ) -> f64 {
+        let mut local_min = HIGHEST_SECURITY as f64;
+        // Try all possible values of e2.
+        for e2 in 0..p2 {
+            let p1 = 2 * (p2 - e2);
+
+            let e1_start = if p1 <= (t / 2) { 0 } else { p1 - t / 2 };
+
+            // Try all possible values of e1.
+            for e1 in e1_start..p1 {
+                let p = 2 * (p1 - e1);
+
+                // The following part is according to Equation (6), Page 11 in this [paper](https://eprint.iacr.org/2013/162.pdf).
+                assert!(k + l >= p2);
+                assert!(k + l >= p1);
+                assert!(p2 >= e2);
+                assert!(p1 >= e1);
+                // Proposition 1 in this [paper](https://eprint.iacr.org/2013/162.pdf).
+                let log_mu2 = Self::cal_comb(p2, e2).log2()
+                    + Self::cal_comb(k + l - p2, p2 - e2).log2()
+                    - Self::cal_comb(k + l, p2).log2();
+
+                let r2: Float = log_mu2 + 4 * (Self::cal_comb((k + l) / 2, p2 / 2).log2())
+                    - Self::cal_comb(k + l, p1).log2();
+
+                let r2: usize = if r2 <= 0 {
+                    0
+                } else if r2 >= l {
+                    l - 1
+                } else {
+                    r2.to_f64() as usize
+                };
+
+                // Also use the equation of mu_1 in Proposition 1.
+                let r = Self::cal_comb(p1, e1).log2()
+                    + Self::cal_comb(k + l - p1, p1 - e1).log2()
+                    + Self::cal_comb(k + l, p1).log2()
+                    - Self::cal_comb(k + l, p).log2();
+
+                let r1 = r - r2;
+                let r1 = if r1 <= 0 {
+                    0
+                } else if (r1.clone() + r2) >= l {
+                    l - r2 - 1
+                } else {
+                    r1.to_f64() as usize
+                };
+
+                let middle = Self::sub_bjmm_isd_binary(n, k, t, p2, l, e1, e2, r1, r2, local_min);
+
+                if middle < local_min {
+                    local_min = middle;
+                }
+            }
+        }
+        local_min
+    }
+
+    // Minimize sub_bjmm_isd_binary with fixed p2.
+    fn min_sub_bjmm_isd_binary_with_fixed_p2(n: usize, k: usize, t: usize, p2: usize) -> f64 {
+        let mut start = 1;
+        let mut end = (n - k - 2) / 8;
+
+        let mut min_cost = Self::min_sub_bjmm_isd_binary_with_fixed_p2_and_l(n, k, t, p2, start);
+
+        while end - start > 10 {
+            let left = (end - start) / 3 + start;
+            let right = end - (end - start) / 3;
+
+            let min_left = Self::min_sub_bjmm_isd_binary_with_fixed_p2_and_l(n, k, t, p2, left);
+            let min_right = Self::min_sub_bjmm_isd_binary_with_fixed_p2_and_l(n, k, t, p2, right);
+
+            if min_left > min_right {
+                start = left;
+                min_cost = min_right;
+            } else {
+                end = right;
+                min_cost = min_left;
             }
         }
 
+        for l in (start + 1)..(end + 5) {
+            let min_middle = Self::min_sub_bjmm_isd_binary_with_fixed_p2_and_l(n, k, t, p2, l);
+            if min_middle < min_cost {
+                min_cost = min_middle;
+            }
+        }
+        min_cost
+    }
+
+    /// The security of the lpn parameters under BJMM_ISD attack for binary field. See Therem 16 in this [paper](https://eprint.iacr.org/2022/712.pdf).
+    ///
+    /// # Arguments.
+    ///
+    /// * `n` - The number of samples.
+    /// * `k` - The length of the secret.
+    /// * `t` - The Hammin weight of the error.
+    pub fn security_under_bjmm_isd_binary(n: usize, k: usize, t: usize) -> f64 {
+        let mut res = Self::min_sub_bjmm_isd_binary_with_fixed_p2(n, k, t, 0);
+
+        for p2 in 1..t {
+            let min = Self::min_sub_bjmm_isd_binary_with_fixed_p2(n, k, t, p2);
+            if min < res {
+                res = min;
+            }
+        }
         res
     }
 }
@@ -122,7 +317,7 @@ impl LpnEstimator {
 mod tests {
     #[test]
     fn security_test() {
-        let security = crate::LpnEstimator::security_under_sd_isd_binary(1 << 14, 3482, 198);
+        let security = crate::LpnEstimator::security_under_bjmm_isd_binary(1 << 10, 100, 10);
         println!("{:?}", security);
     }
 }
